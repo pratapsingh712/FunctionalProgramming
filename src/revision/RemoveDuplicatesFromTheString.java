be55fec42afe211f12a2326dbe6cc4edce027c84
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2026-02-22 17:45:12 +0530

    minor changes

--- src/revision/RemoveDuplicatesFromTheString.java
+++ src/revision/RemoveDuplicatesFromTheString.java
@@ -10,9 +10,15 @@ public class RemoveDuplicatesFromTheString {
 
         // let's convert the string into stream and get back the resume
 
-        String result = Arrays.stream(s.split(""))
-                .distinct().collect(Collectors.joining());
+//        String result = Arrays.stream(s.split(""))  performance overhead as stream of string will be created thus n string objects
+//                .distinct().collect(Collectors.joining());
+
+        String result = s.chars()
+                        .distinct()
+                                .mapToObj(c->String.valueOf((char)c))
+                                        .collect(Collectors.joining());
 
         System.out.println("Word without duplicates :"+result);
+
     }
 }
